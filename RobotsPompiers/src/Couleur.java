cc0baeb0315733e4426996dc3377f05ada6bbb0c
Carlos Mendes <carlosmendes_7@hotmail.com>
2018-11-10 02:55:09 +0100

    Update and rename Color.java to Couleur.java

--- RobotsPompiers/src/Couleur.java
+++ RobotsPompiers/src/Couleur.java
@@ -1,7 +1,7 @@
-public enum Color{
+public enum Couleur{
  RED("#f43827"), BLUE("#1123c4"), GREEN("#20541c"), BROWN("#3d2d22"), WHITE("#ffffff"), PURPLE("#d14fc4");
 
- private Color(String color){
+ private Couleur(String color){
     this.color = color;
   }
 
